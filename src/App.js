529b19d8a17bc3aa50fe392ca182edfe9be5dd5d
powerbarrusa <barr@covrigaru.com>
2019-02-04 16:02:56 -0700

    minor stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -8,7 +7,7 @@ class App extends Component {
       <div className="App">
         Test
       </div>
-    );
+    )
   }
 }
 
