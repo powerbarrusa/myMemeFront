12eb0fcda9e146b55d12613c8bc96a5e95cd348d
Cesar Montoya <cesarm@Cesars-MacBook-Pro.local>
2019-02-05 16:00:41 -0700

    added meme collection page

--- src/components/yourMemes.js
+++ src/components/yourMemes.js
@@ -6,9 +6,27 @@ NavLink
 class YourMemes extends Component {
 
   render() {
+    console.log(this.props.memes)
     return (
       <div className="App">
         <NavLink exact to="/"><i className="material-icons">home</i></NavLink>
+        {this.props.memes ? this.props.memes.map(meme => {
+          return (
+            <div class="row">
+              <div class="col s12 m7">
+                <div class="card">
+                  <div class="card-image">
+                    <h1 className="topText">Card Title</h1>
+                    <img className="image" src={meme.image_url}></img>
+                    <h1 className="bottomText">Card Title</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+        )
+        }) :
+        ""
+      }
 
       </div>
     );
