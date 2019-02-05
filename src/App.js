71a285a1cc25a522e9193cd55ca8e6fe3128b523
Cesar Montoya <cesarm@Cesars-MacBook-Pro.local>
2019-02-05 11:49:06 -0700

    pulled

--- src/App.js
+++ src/App.js
@@ -24,18 +24,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
-        {this.state.memes ? <img src={this.state.memes[0].image_url}></img> : ""}
-        <a href="form.js" class="btn-floating btn-large waves-effect waves-light red" onSubmit="return false"><i class="material-icons" >add</i></a>
-
-=======
         <div className="row">
           <p className="col s8 m4 offset-s2 offset-m4">myMeme</p>
           <a href="/#" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn red">button</a>
           <a href="/#" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn blue">button</a>
           <a href="/#" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn green">button</a>
         </div>
->>>>>>> 922d4e11db188ef78e885b54f15b749628e790ac
+
       </div>
     )
   }
