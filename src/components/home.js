abf5af7f0cba9608da500943955f7b0e07bf29b5
Cesar Montoya <cesarm@Cesars-MacBook-Pro.local>
2019-02-05 15:02:35 -0700

    fixed warnings

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
-Route,
-NavLink,
-BrowserRouter,
-Switch,
-Redirect
-} from "react-router-dom";
+NavLink
+} from "react-router-dom"
 
 class Home extends Component {
 
@@ -13,12 +9,12 @@ class Home extends Component {
     return (
       <div className="row">
         <p className="col s8 m4 offset-s2 offset-m4">myMeme</p>
-        <a className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn red"><NavLink exact to="/form">Make a Meme</NavLink></a>
-        <a className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn blue"><NavLink exact to="/yourMemes">Your Memes</NavLink></a>
-        <a className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn green"><NavLink exact to="/randomMeme">Random Meme</NavLink></a>
+        <NavLink exact to="/form" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn red">Make a Meme</NavLink>
+        <NavLink exact to="/yourMemes" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn blue">Your Memes</NavLink>
+        <NavLink exact to="/randomMeme" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn green">Random Meme</NavLink>
       </div>
     );
   }
 }
 
-export default Home;
+export default Home
--- src/components/randomMeme.js
+++ src/components/randomMeme.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
-Route,
-NavLink,
-BrowserRouter,
-Switch,
-Redirect,
-Link
-} from "react-router-dom";
+NavLink
+} from "react-router-dom"
 
 class RandomMeme extends Component {
 
   render() {
     return (
       <div className="App">
-        <NavLink exact to="/"><i class="material-icons">home</i></NavLink>
+        <NavLink exact to="/"><i className="material-icons">home</i></NavLink>
 
       </div>
     );
   }
 }
 
-export default RandomMeme;
+export default RandomMeme
