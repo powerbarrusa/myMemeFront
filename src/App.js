922d4e11db188ef78e885b54f15b749628e790ac
powerbarrusa <barr@covrigaru.com>
2019-02-05 11:42:37 -0700

    homepage bones styling

--- src/App.js
+++ src/App.js
@@ -9,20 +9,27 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    fetch('http://localhost:3001')
-    .then(data => data.json())
-    .then(memes => {
+  async componentDidMount(){
+    try {
+      const api = await fetch('http://localhost:3001')
+      const memes = await api.json()
       this.setState({
         memes: memes
       })
-    })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   render() {
-    console.log(this.state.memes)
     return (
       <div className="App">
+        <div className="row">
+          <p className="col s8 m4 offset-s2 offset-m4">myMeme</p>
+          <a href="/#" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn red">button</a>
+          <a href="/#" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn blue">button</a>
+          <a href="/#" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn green">button</a>
+        </div>
       </div>
     )
   }
