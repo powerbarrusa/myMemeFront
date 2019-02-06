a21aab12d78cd043aaf3361453e8912c8aef4dd3
Logan Trujillo <logantrujillo@Logans-MacBook-Air.local>
2019-02-06 11:28:43 -0700

    Moved state to app.js

--- src/App.js
+++ src/App.js
@@ -14,9 +14,30 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      memes: ""
+      memes: "",
+      top_text: "",
+      bottom_text: "",
+      image_url: "",
     }
+    this.onSubmit = this.onSubmit.bind(this)
   }
+
+  onChangeTopText = (e) => {
+    this.setState({
+      top_text: e.target.value,
+    })
+  }    
+  onChangeBottomText = (e) => {
+    this.setState({
+      bottom_text: e.target.value,
+    })
+  }    
+  onChangeImgUrl = (e) => {
+    this.setState({
+      image_url: e.target.value,
+    })
+  }
+
 
   async componentDidMount(){
     try {
@@ -30,6 +51,32 @@ class App extends Component {
     }
   }
 
+  onSubmit () {
+    var url = 'http://localhost:3001/createMeme'
+    var data = {
+      top_text: this.state.top_text,
+      bottom_text: this.state.bottom_text,
+      image_url: this.state.image_url
+    }
+    console.log("about to fetch", data)
+    fetch(url, {
+
+      method: 'POST', // or 'PUT'
+      mode: 'cors',
+      body: JSON.stringify(data), // data can be `string` or {object}!
+      headers: {
+        'Content-Type': 'application/json'
+      }
+      }).then(res => res.json())
+        .then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error:', error));
+        let newMemes = [...this.state.memes]
+        newMemes.push(data)
+        this.setState({
+          memes: newMemes
+        })
+    }
+
   render() {
     return (
       <BrowserRouter>
@@ -37,10 +84,14 @@ class App extends Component {
           <div className="content">
             <Switch>
               <Route path="/" exact render={() => <Home/>}/>
-              <Route path="/createMeme" render={() => <CreateMeme/>}/>
-
-              
-             
+              <Route path="/createMeme" render={() => 
+                <CreateMeme 
+                  ChangeTop ={this.onChangeTopText} 
+                  ChangeBottom ={this.onChangeBottomText} 
+                  ChangeImg ={this.onChangeImgUrl} 
+                  Submit={this.onSubmit}
+                  />
+                }/>
 
               <Route path="/yourMemes" render={() => <YourMemes memes={this.state.memes}/>}/>
               <Route path="/randomMeme" render={() => <RandomMeme state={this.state.memes}/>}/>
--- src/components/createMeme.js
+++ src/components/createMeme.js
@@ -5,82 +5,33 @@ NavLink
 
 class CreateMeme extends Component {
 
-  constructor() {
-    super()
-    this.state = {
-      top_text: "",
-      bottom_text: "",
-      image_url: "",
-
-    }
-    this.onChangeTopText = this.onChangeTopText.bind(this)
-    this.onChangeBottomText = this.onChangeBottomText.bind(this)
-    this.onChangeImgUrl = this.onChangeImgUrl.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
-  }
-
-  onChangeTopText(e) {
-    this.setState({
-      top_text: e.target.value,
-    })
-  }    
-  onChangeBottomText(e) {
-    this.setState({
-      bottom_text: e.target.value,
-    })
-  }    
-  onChangeImgUrl(e) {
-    this.setState({
-      image_url: e.target.value,
-    })
-  }
-  
-  onSubmit() {
-    var url = 'http://localhost:3001/createMeme'
-    var data = this.state
-    console.log("about to fetch", data)
-  fetch(url, {
-
-    method: 'POST', // or 'PUT'
-    body: JSON.stringify(data), // data can be `string` or {object}!
-    headers: {
-      'Content-Type': 'application/json'
-    }
-    }).then(res => res.json())
-      .then(response => console.log('Success:', JSON.stringify(response)))
-      .catch(error => console.error('Error:', error));
-  }
-
-
   render() {
-  
     return (
       <div className="App">
         <NavLink exact to="/"><i className="material-icons">home</i></NavLink>
         <div className="column">
-          
             <div>
               <div className="input-field col s6">
-                <input id="top-text" type="text" className="validate" onChange={this.onChangeTopText}></input>
+                <input id="top-text" type="text" className="validate" onChange={this.props.ChangeTop}></input>
                 <label htmlFor="top-text">Top Text</label>
               </div>
               <div className="input-field col s6">
-                <input id="bottom-text" type="text" className="validate" onChange={this.onChangeBottomText}></input>
+                <input id="bottom-text" type="text" className="validate" onChange={this.props.ChangeBottom}></input>
                 <label htmlFor="bottom-text">Bottom Text</label>
               </div>
               <div className="input-field col s6">
-                <input id="image-url" type="text" className="validate" onChange={this.onChangeImgUrl}></input>
+                <input id="image-url" type="text" className="validate" onChange={this.props.ChangeImg}></input>
                 <label htmlFor="image-url">Image_URL</label>
               </div>
             </div>
-            <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.onSubmit}>Submit
-              <i className="material-icons right">send</i>
-            </button>
-         
+            <NavLink exact to="/yourMemes">
+              <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.props.Submit}>Submit
+                <i className="material-icons right">send</i>
+              </button>
+            </NavLink>
           </div>
         </div>
     );
   }
 }
-
 export default CreateMeme
