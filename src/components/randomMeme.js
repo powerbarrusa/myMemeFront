b10d582efd5efaf85ba10aec878247b9890f620c
powerbarrusa <barr@covrigaru.com>
2019-02-05 22:15:33 -0700

    hard-wiring image url works. for some reason can't grab from state

--- src/components/randomMeme.js
+++ src/components/randomMeme.js
@@ -10,24 +10,24 @@ class RandomMeme extends Component {
     return Math.floor(Math.random() * (max - min)) + min
   }
 
+
+
   render() {
     console.log(this.props.state)
+    console.log(this.props.state[0])
     return (
       <div className="App">
         <NavLink exact to="/"><i className="material-icons">home</i></NavLink>
-        <p><a href="/#" className="waves-effect waves-light btn red">Hit me with another!</a></p>
+        <p><button type="button" className="waves-effect waves-light btn red">Hit me with another!</button></p>
         <div className="row">
           <div className="col s12">
             <div className="card">
               <div className="card-image">
-                <img src="images/sample-1.jpg" alt="" />
+                {/* <img src={this.props.state[1].image} alt="" /> */}
                 <span className="card-title">Card Title</span>
               </div>
               <div className="card-content">
                 <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-              </div>
-              <div className="card-action">
-                <a href="/#">This is a link</a>
               </div>
             </div>
           </div>
