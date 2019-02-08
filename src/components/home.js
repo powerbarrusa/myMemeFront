10f04493db5f692d4b7e2c5799668360c32bcc42
powerbarrusa <barr@covrigaru.com>
2019-02-07 20:12:18 -0700

    new logo?

--- src/components/home.js
+++ src/components/home.js
@@ -8,10 +8,8 @@ class Home extends Component {
   render() {
     return (
       <div className="row">
-        <p className="col s8 m4 offset-s2 offset-m4">myMeme</p>
-
+        <img src="https://fontmeme.com/permalink/190208/e53fe677035baef6db80bfaa9103baf3.png" alt="sabo-font" border="0" />
         <NavLink exact to="/createMeme" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn red">Make a Meme</NavLink>
-
         <NavLink exact to="/yourMemes" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn blue">Your Memes</NavLink>
         <NavLink exact to="/randomMeme" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn green">Random Meme</NavLink>
       </div>
