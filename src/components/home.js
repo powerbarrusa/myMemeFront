23486bd1ece78f06834b33a17f7e084f464c3de6
powerbarrusa <barr@covrigaru.com>
2019-02-08 10:41:45 -0700

    working version

--- src/components/home.js
+++ src/components/home.js
@@ -7,16 +7,6 @@ class Home extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
-      <div className="row">
-        <p className="col s8 m4 offset-s2 offset-m4">myMeme</p>
-
-        <NavLink exact to="/createMeme" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn red" onClick={this.props.statusAdd}>Make a Meme</NavLink>
-        <NavLink exact to="/yourMemes" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn blue">Your Memes</NavLink>
-        <NavLink exact to="/randomMeme" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn green">Random Meme</NavLink>
-      </div>
-    );
-=======
         <div className="row">
           <img src="https://fontmeme.com/permalink/190208/e53fe677035baef6db80bfaa9103baf3.png" alt="sabo-font" border="0" />
           <NavLink exact to="/createMeme" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn red">Make a Meme</NavLink>
@@ -24,7 +14,6 @@ class Home extends Component {
           <NavLink exact to="/randomMeme" className="z-depth-5 col s8 m4 offset-s2 offset-m4 waves-effect waves-light btn green">Random Meme</NavLink>
         </div>
     )
->>>>>>> barr
   }
 }
 
